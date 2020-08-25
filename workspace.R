06cfbfbe493cd90b38bec5c400cee37c221b53b8
Filipe Tavares-Cadete <filipe@tavares.cadete.eu>
2020-08-25 21:57:16 +0000

    First draft of operator.

--- workspace.R
+++ workspace.R
@@ -1,15 +1,34 @@
-library(tercen)
+library(scRNAseq)
+library(SingleCellExperiment)
+library(scater)
+library(tidyr)
 library(dplyr)
+library(tercen)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "7d6077b7fa4df6315a718714de00346e")
+options("tercen.stepId"     = "e8df35d9-2f50-45e0-a192-671c49c34418")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
+(ctx = tercenCtx())
+
+logged_values <- as.logical(ctx$op.value('logged_values'))
+centre_size_factors <- as.logical(ctx$op.value('centre_size_factors'))
+
+count_matrix <- ctx$as.matrix()
+
+logcounts <- normalizeCounts(count_matrix,
+                             librarySizeFactors(sce),
+                             centre_size_factors = centre_size_factors,
+                             return_log = logged_values)
+
+output <- logcounts %>%
+  as.data.frame() %>%
+  mutate(.ri = 0:(n()-1)) %>%
+  gather(key = "column", value = "logcounts", -.ri) %>%
+  mutate(.ci = as.integer(stringr::str_remove(column, "V")) - 1) %>%
+  select(-column)
+
+ctx$addNamespace(output) %>%
   ctx$save()
